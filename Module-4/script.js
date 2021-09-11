326428d4ebeaa356f47366561517ccfd2ce4419b
Mohammed Sameer S <69456489+host712@users.noreply.github.com>
2021-09-11 09:55:46 +0530

    Add files via upload

--- Module-4/script.js
+++ Module-4/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Disha", "Kriti", "Esha", "Shraddha", "Nikki", "Amyra", "Pooja", "Neha", "Vartika", "Tanvi"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
